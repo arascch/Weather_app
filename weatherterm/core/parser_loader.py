9249de70fc4638c6bb9df76bc26ecc8b90ceefbe
arasch <arash.ask.94@gmail.com>
2019-10-18 14:00:20 +0330

    create parser_loader file and add coder

--- weatherterm/core/parser_loader.py
+++ weatherterm/core/parser_loader.py
@@ -1,3 +1,28 @@
 import os 
 import re
 import inspect
+
+def _get_parser_list(dirname):
+    files = [f.replace('.py' , '')
+             for f in os.listdir(dirname)
+             if not f.startwith('__')]
+            
+    return files
+
+def _import_parsers(parsersfiles):
+    m = re.compile('.+parser$' , re.I)
+    _modules = __import__('weatherterm.parsers' , globals() , locals() , parsersfiles , 0 )
+    _parsers = [(k, v) for k , v in inspect.getmembers(_modules)
+                if inspect.ismodule(v) and m.match(k)]
+    _classes = dict()
+    
+    for k , v in _parsers:
+        _classes.update({k : v for k , v in inspect.getmembers(v)
+                         if inspect.isclass(v) and m.match(k)})   
+
+    return _classes
+
+
+def load(dirname):
+    parsersfiles = _get_parser_list(dirname)
+    return _import_parsers(parsersfiles)
